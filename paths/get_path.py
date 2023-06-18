cb42c0daee69432e9349b20fdbd1aedf0a02199c
gubarevdanil <danilgubarev0498@gmail.com>
2023-06-18 14:12:03 +0300

    four

---
+++ paths/get_path.py
@@ -0,0 +1,8 @@
+from os.path import abspath
+
+def get_full_path(path):
+    full_path = abspath(__file__ + '/../..') + f"/{path}"
+    # full_path = os.path.join(os.path.abspath(__file__ + '/../..'), path)
+    return full_path
+
+# print(get_full_path('images\\menu\\play.png'))
